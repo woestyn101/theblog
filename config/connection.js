3ff5ee7210a3cf525dbf3038fc710cbaf2c2b8df
Marius Du Plooy <woestyn101@gmail.com>
2024-02-29 16:27:05 -0500

    connection config

--- config/connection.js
+++ config/connection.js
@@ -2,18 +2,21 @@ const Sequelize = require('sequelize');
 
 // Enable access to .env variables
 require('dotenv').config();
+let sequelize;
 
-// Use environment variables to connect to database
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306
-  }
-);
-//console.log(process.env.DB_PASSWORD)
+if (process.env.CLEARDB_DATABASE_URL) {
+  sequelize = new Sequelize(process.env.CLEARDB_DATABASE_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306
+    }
+  );
+}
 
 module.exports = sequelize;
