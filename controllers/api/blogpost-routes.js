1250b8043224183d0da2c87539421721f55e0657
Marius Du Plooy <woestyn101@gmail.com>
2024-03-03 23:48:47 -0500

    see individual post

--- controllers/api/blogpost-routes.js
+++ controllers/api/blogpost-routes.js
@@ -21,5 +21,27 @@ router.post('/', (req, res) => {
       });
   });
 
+  router.get('/:id', async (req, res) => {
+    try {
+      const projectData = await Blogpost.findByPk(req.params.id, {
+        include: [
+          {
+            model: User,
+           // attributes: ['name'],
+          },
+        ],
+      });
+  
+      const project = projectData.get({ plain: true });
+  
+      res.render('blogpost', {
+        ...project,
+        logged_in: req.session.logged_in
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
 
 module.exports = router;
